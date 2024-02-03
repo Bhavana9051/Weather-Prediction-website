4db8b4cc1d674c5efaa2e0b819094ec1e15662b9
Bhavana Matala <97403784+Bhavana9051@users.noreply.github.com>
2024-02-03 20:32:53 +0530

    Update script.js
    path updated again

--- assets/script.js
+++ assets/script.js
@@ -69,6 +69,6 @@ function setBackgroundImage(weatherDescription) {
     }
 
     const imageFilename = weatherImages[matchedWeatherType];
-    const imagePath = `./imgs/${imageFilename}`;
+    const imagePath = `assets/imgs/${imageFilename}`;
     bodyElement.style.backgroundImage = `url('${imagePath}')`;
 }
